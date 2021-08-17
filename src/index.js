2fce65f1679d62abc2fad57d6308b9d4f4a81907
Anniyak <anniyak@gmail.com>
2021-08-17 08:45:57 +0300

    init: start expression-calculator task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 function eval() {
-    // Do not use eval!!!
-    return;
+  // Do not use eval!!!
+  return;
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+  expr = expr.replace(/\s+/g, '');
+  let operators = ['+', '-', '*', '/'];
+  function calc(exp) {
+
+    if (+exp !== NaN) return +exp;
+    const start = exp.indexOf('(');
+    if (start !== -1) {
+      const end = exp.indexOf(')');
+
+    }
+  }
+  return expressionCalculator(expr);
 }
 
 module.exports = {
-    expressionCalculator
+  expressionCalculator
 }
