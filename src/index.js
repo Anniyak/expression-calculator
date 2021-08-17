8e3671c8bb7a6a3bb2327707a8fb9f542c06f043
Anniyak <anniyak@gmail.com>
2021-08-17 11:59:03 +0300

    feat: add more logic

--- src/index.js
+++ src/index.js
@@ -11,7 +11,18 @@ function expressionCalculator(expr) {
     if (+exp !== NaN) return +exp;
     const start = exp.indexOf('(');
     if (start !== -1) {
+
       const end = exp.indexOf(')');
+      const newExp = exp.substring(start + 1, end - 1);
+      const startOperation = start > 0 ? exp[start - 1] : null;
+      const endOperation = end < exp.length() - 1 ? exp[end + 1] : null;
+      if (startOperation == '*' || startOperation == '/') {
+
+      }
+
+      else {
+      }
+      //if (start == 0 && end == exp.length() - 1) { calc(newExp); }
 
     }
   }
